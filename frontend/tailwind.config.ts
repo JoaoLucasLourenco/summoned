42743ac70a1d29d4f9e1114a9960d9d1ec867801
João Lucas Lourenço <jao.lucas.lourenco@gmail.com>
2024-08-21 14:08:16 -0300

    feat: theme feat: login page

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -6,7 +6,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      
+      colors: {
+        text: 'var(--color-text)',
+        error: 'var(--color-error)',
+        paper: 'var(--color-bg-paper)',
+        primary: 'var(--color-primary)',
+        secondary:'var(--color-secondary)',
+        warning:'var(--color-warning)',
+        success:'var(--color-success)',
+        default:'var(--color-default)',
+        'primary-contrast': 'var(--color-primary-contrast)',
+      }
     },
   },
   plugins: [],
