5cc518ece419c13a053f456ad0eb3c78b98f2b62
João Lucas Lourenço <jao.lucas.lourenco@gmail.com>
2024-09-16 14:58:03 -0300

    refactor: singin form

--- frontend/src/components/login/singinForm.tsx
+++ frontend/src/components/login/singinForm.tsx
@@ -1,10 +1,16 @@
 'use client';
-import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import { userLoginValidatorSchema } from '@/types/validation/validation';
+import InputSingUpInForm from "../register/inputSingUp";
 export default function SingInForm(){
-    const {register} = useForm();
-    const [email, setEmail] = useState('');
-    const [senha, setSenha] = useState('');
+    const { reset, register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(userLoginValidatorSchema)
+    });
+
+    function submit(data: object) {
+        console.log(data);
+    }
     return(
         <main className="
         main-singInUp
@@ -22,25 +28,60 @@ export default function SingInForm(){
                 Login
             </h1>
             
-            <form action="" className="
+            <form action="" onSubmit={handleSubmit(submit)} className="
 
             ">
-                <label htmlFor="email">Email: </label>
+                <InputSingUpInForm
+                        label='Email'
+                        name='email'
+                        placeholder='seu@email.aqui'
+                        type='text'
+                        register={register}
+                        icon={
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                width="20"
+                                height="20"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="1"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                className="lucide lucide-mail">
+                                <rect width="20" height="16" x="2" y="4" rx="2" />
+                                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+                            </svg>
+                        }
+                    />
+                    <div className="error-div">
+                        {errors.email?.message}
+                    </div>
 
-                <input type="email"
-                id="email"
-                className="
-                textInput-singInUp
-                "
-                value={email}
-                placeholder={"Digite seu email aqui"}
-                onChange={(e)=>setEmail(e.target.value)}
-                />
-
-                <label htmlFor="senha" className="
-                
-                ">
-                    Senha: </label>
+                    <InputSingUpInForm
+                        label='Senha'
+                        name='senha'
+                        placeholder='Digite sua senha aqui'
+                        type='password'
+                        register={register}
+                        icon={
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                width="20"
+                                height="20"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="1"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                className="lucide lucide-lock">
+                                <rect width="18" height="11" x="3" y="11" rx="2" ry="2" />
+                                <path d="M7 11V7a5 5 0 0 1 10 0v4" />
+                            </svg>
+                        }
+                    />
+                    <div className='error-div'>
+                        {errors.senha?.message}
+                    </div>
                 <a 
                 href=""
                 
@@ -52,15 +93,7 @@ export default function SingInForm(){
                 ">
                     Esqueceu a senha? </a>
 
-                <input type="password"
-                id="senha"
-                className="
-                textInput-singInUp
-                "
-                value={senha}
-                placeholder={"Digite sua senha aqui"}
-                onChange={(e)=>setSenha(e.target.value)}
-                />
+                
                 
                 <button
                 className="
